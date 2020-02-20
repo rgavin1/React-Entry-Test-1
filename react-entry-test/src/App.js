0a7c692673eaf0a8c6d27fb7227beaaa91af5680
rgavin1 <44215916+rgavin1@users.noreply.github.com>
2020-02-20 06:05:58 -0500

    commit init

--- react-entry-test/src/App.js
+++ react-entry-test/src/App.js
@@ -32,33 +32,80 @@ class App extends React.Component {
       numberHeader: 0,
       log: "Started Log at 0",
     };
+    this.randomNumber = this.randomNumber.bind(this);
+    this.incrementNumber = this.incrementNumber.bind(this);
+    this.evaluateNumber = this.evaluateNumber.bind(this);
   }
 
 
 // Add a Function to be called when the Random Number is clicked to change the Number Header State
 // The random number needs to be between 0 and 100 and showing no decimals 
 // Add to the log text area saying "Random Number to " and then the new number generated on a new line.  
+randomNumber = () => {
+  //Select random number between 0 and 100
+  const randomInteger = Math.floor(Math.random() * 101);
 
+  this.setState({ 
+    numberHeader: randomInteger,
+    log: `${this.state.log}\nRandom Number to ${randomInteger}`,
+  });
+}
 
 // Add a Function to be called when the Increment Number is clicked to change the Number Header State
 // If the number will be above 100 (an increment from 100 to 101) display an error stating that the maximum number is 100 
 // Add to the log text area saying "Incremented Number to " and then the new number generated on a new line.  
+incrementNumber = () => {
+  const { numberHeader, log } = this.state;
+  const newInteger = numberHeader + 1;
 
+  //If new integer is larger than 100 keep number header at 100 and display message
+  newInteger > 100 ?
+    this.setState({
+      numberHeader: 100,
+    }, () => { alert("Maximum number is 100") })
+  :
+  this.setState({ 
+    numberHeader: newInteger, 
+    log: `${log}\nIncremented Number to ${newInteger}`,
+  });
+
+}
 
 
 // If the number is greater than or equal to 50, then the number header needs to be a red. If the number is below 50, then the number header needs to be a blue. 
+evaluateNumber = () => {  
+  if (this.state.numberHeader >= 50) {
+    return 'red';
+  } else {
+    return 'blue';
+  }
+
+}
 
   render() {
+
+    const { log } = this.state;
+
     return (
       <div className="App">
-          <h1>{this.state.numberHeader}</h1>
+          <h1 style={{
+            color: this.evaluateNumber()
+          }}>{this.state.numberHeader}</h1>
         
-          <button> Random Number</button> &nbsp;
-          <button> Increment Number</button>
+          <button onClick={this.randomNumber}> Random Number</button> &nbsp;
+          <button onClick={this.incrementNumber}> Increment Number</button>
           <br></br>
           <br></br>
-          {/* Add a text area for the log */}
-          <p> By: Your Name - Date Test was Taken</p>
+          
+          <textarea
+          name="log" 
+          rows="10" 
+          cols="100"
+          value={log}
+          readOnly 
+          >Log</textarea>
+          
+          <p> By: Ramsey Gavin - 02/19/2020</p>
           <p>	&#169; 2019 World Shipping, Inc.</p>
       </div>
     );
